aa5d74261d27f2485e1e6e03748cbedfc6a5b8b2
Hossein <rezaeig22@gmail.com>
2024-01-30 13:53:24 +0330

    Set Up Yekan Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,14 @@ import '@radix-ui/themes/styles.css';
 import './theme-config.css';
 import './globals.css';
 import type { Metadata } from 'next';
-import { Theme, ThemePanel } from '@radix-ui/themes';
+import { Theme } from '@radix-ui/themes';
 import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
+
 import NavBar from './NavBar';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const Yekan = localFont({ src: '../public/Yekan.ttf', variable: '--font-yekan' });
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -16,7 +19,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.variable} dir="rtl">
+			<body className={Yekan.variable} dir="rtl">
 				<Theme accentColor="violet">
 					<NavBar />
 					<main className="p-5">{children}</main>
