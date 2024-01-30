942e7bd01552640129689feb8a86d62f1cab07a6
Hossein <rezaeig22@gmail.com>
2024-01-30 14:58:23 +0330

    19. 10- Handling Errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import z from 'zod';
 import prisma from '@/prisma/client';
 
 const createIssueSchema = z.object({
-	title: z.string().min(1).max(255),
-	description: z.string().min(1)
+	title: z.string().min(1, 'Title is required.').max(255),
+	description: z.string().min(1, 'description is required.').max(255)
 });
 
 export const POST = async (req: NextRequest) => {
@@ -14,7 +14,7 @@ export const POST = async (req: NextRequest) => {
 	// VALIDATE BODY
 	const validation = createIssueSchema.safeParse(body);
 	if (!validation.success) {
-		return NextResponse.json(validation.error.errors, { status: 400 });
+		return NextResponse.json(validation.error.format(), { status: 400 });
 	}
 
 	// CREATING ISSUE
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { Button, TextField } from '@radix-ui/themes';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import { Controller, useForm } from 'react-hook-form';
-import axios from 'axios';
-import { useMemo } from 'react';
+import axios, { AxiosError } from 'axios';
+import { useMemo, useState } from 'react';
 import 'easymde/dist/easymde.min.css';
 
 interface IssueForms {
@@ -13,9 +13,10 @@ interface IssueForms {
 	description: string;
 }
 
-const IssuePage = () => {
+const NewIssuePage = () => {
 	const router = useRouter();
 	const { register, control, handleSubmit } = useForm<IssueForms>();
+	const [error, setError] = useState('');
 
 	const optionsSimpleMDE = useMemo(() => {
 		return {
@@ -25,30 +26,41 @@ const IssuePage = () => {
 	}, []);
 
 	const onSubmit = async (data: IssueForms) => {
-		await axios.post('/api/issues', data);
-		router.push('/issues');
+		try {
+			await axios.post('/api/issues', data);
+			router.push('/issues');
+		} catch (error) {
+			setError('یک خطای غیرمنتظره رخ داد!');
+		}
 	};
 
 	return (
-		<form className="max-w-xl space-y-3" onSubmit={handleSubmit(onSubmit)}>
-			<TextField.Root>
-				<TextField.Input placeholder="عنوان" style={{ padding: '5px' }} {...register('title')} />
-			</TextField.Root>
-			<Controller
-				name="description"
-				control={control}
-				render={({ field }) => (
-					<SimpleMDE
-						placeholder="شرح"
-						options={optionsSimpleMDE}
-						style={{ textAlign: 'right' }}
-						{...field}
-					/>
-				)}
-			/>
-			<Button>ارسال مشکل جدید</Button>
-		</form>
+		<div className="max-w-xl">
+			{error && (
+				<Callout.Root color="red" className="mb-5">
+					<Callout.Text>{error}</Callout.Text>
+				</Callout.Root>
+			)}
+			<form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+				<TextField.Root>
+					<TextField.Input placeholder="عنوان" style={{ padding: '5px' }} {...register('title')} />
+				</TextField.Root>
+				<Controller
+					name="description"
+					control={control}
+					render={({ field }) => (
+						<SimpleMDE
+							placeholder="شرح"
+							options={optionsSimpleMDE}
+							style={{ textAlign: 'right' }}
+							{...field}
+						/>
+					)}
+				/>
+				<Button>ارسال مشکل جدید</Button>
+			</form>
+		</div>
 	);
 };
 
-export default IssuePage;
+export default NewIssuePage;
