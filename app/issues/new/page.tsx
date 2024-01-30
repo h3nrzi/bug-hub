d12896fa77683a6c885efdbb829132698dc82108
Hossein <rezaeig22@gmail.com>
2024-01-30 10:26:51 +0330

    18. 9- Handling Form Submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,26 +1,53 @@
 'use client';
 
-import { TextField, Button } from '@radix-ui/themes';
+import { useRouter } from 'next/navigation';
+import { Button, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
-import 'easymde/dist/easymde.min.css';
+import { Controller, useForm } from 'react-hook-form';
+import axios from 'axios';
 import { useMemo } from 'react';
+import 'easymde/dist/easymde.min.css';
+
+interface IssueForms {
+	title: string;
+	description: string;
+}
 
 const IssuePage = () => {
-	const options = useMemo(() => {
+	const router = useRouter();
+	const { register, control, handleSubmit } = useForm<IssueForms>();
+
+	const optionsSimpleMDE = useMemo(() => {
 		return {
 			autofocus: true,
 			spellChecker: false
 		};
 	}, []);
 
+	const onSubmit = async (data: IssueForms) => {
+		await axios.post('/api/issues', data);
+		router.push('/issues');
+	};
+
 	return (
-		<div className="max-w-xl space-y-3">
+		<form className="max-w-xl space-y-3" onSubmit={handleSubmit(onSubmit)}>
 			<TextField.Root>
-				<TextField.Input placeholder="عنوان" style={{ padding: '5px' }} />
+				<TextField.Input placeholder="عنوان" style={{ padding: '5px' }} {...register('title')} />
 			</TextField.Root>
-			<SimpleMDE placeholder="شرح" options={options} style={{ textAlign: 'right' }} />
+			<Controller
+				name="description"
+				control={control}
+				render={({ field }) => (
+					<SimpleMDE
+						placeholder="شرح"
+						options={optionsSimpleMDE}
+						style={{ textAlign: 'right' }}
+						{...field}
+					/>
+				)}
+			/>
 			<Button>ارسال مشکل جدید</Button>
-		</div>
+		</form>
 	);
 };
 
