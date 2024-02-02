3a046f12d4f60d54256fd18a01d70d03aa903e35
Hossein <rezaeig22@gmail.com>
2024-02-02 21:00:12 +0330

    -- CLEAR --

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,8 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
-import { Card, Flex, Grid, Heading, Text, Box, Button } from '@radix-ui/themes';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import ReactMarkdown from 'react-markdown';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import Link from 'next/link';
-import EditIssueButton from './edit/EditIssueButton';
 import IssueDetails from './IssueDetails';
+import EditIssueButton from './edit/EditIssueButton';
 
 interface Props {
 	params: { id: string };
