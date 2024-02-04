797e54ad84f9fa45af123e7451ceb6c691d09053
Hossein <rezaeig22@gmail.com>
2024-02-04 12:01:16 +0330

    Show confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,34 @@
+'use client';
+
 import { TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 	return (
-		<Button color="red">
-			<TrashIcon />
-			حذف
-		</Button>
+		<AlertDialog.Root>
+			<AlertDialog.Trigger>
+				<Button color="red">
+					<TrashIcon />
+					حذف
+				</Button>
+			</AlertDialog.Trigger>
+			<AlertDialog.Content>
+				<AlertDialog.Title>عملیات حذف را تایید کنید.</AlertDialog.Title>
+				<AlertDialog.Description>
+					آیا مطمئن هستید که می خواهید این باگ را حذف کنید؟ این عمل قابل بازگشت نیست.
+				</AlertDialog.Description>
+				<Flex mt="4" gap="3">
+					<AlertDialog.Cancel>
+						<Button variant="soft" color="gray">
+							انصراف
+						</Button>
+					</AlertDialog.Cancel>
+					<AlertDialog.Action>
+						<Button color="red">تایید</Button>
+					</AlertDialog.Action>
+				</Flex>
+			</AlertDialog.Content>
+		</AlertDialog.Root>
 	);
 };
 
