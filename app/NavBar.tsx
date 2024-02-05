fff5846257add4615b588f5d23ad52cb42a2ebe7
Hossein <rezaeig22@gmail.com>
2024-02-05 19:24:40 +0330

    52. Change the Layout of the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 const NavBar = () => {
 	const currentPath = usePathname();
@@ -17,30 +17,40 @@ const NavBar = () => {
 	];
 
 	return (
-		<nav className="flex gap-6 border-b mb-5 px-5 h-14 items-center">
-			<Link href="/">
-				<AiFillBug />
-			</Link>
-			<ul className="flex gap-6">
-				{links.map((link) => (
-					<li key={link.href}>
-						<Link
-							href={link.href}
-							className={classnames({
-								'text-zinc-900': link.href === currentPath,
-								'text-zinc-500': link.href !== currentPath,
-								'hover:text-zinc-800 transition-colors': true
-							})}
-						>
-							{link.label}
+		<nav className="border-b mb-5 py-4 px-6 h-14">
+			<Container>
+				<Flex justify="between" align="center">
+					<Flex gap="6" align="center">
+						<Link href="/">
+							<AiFillBug />
 						</Link>
-					</li>
-				))}
-			</ul>
-			<Box>
-				{status === 'authenticated' && <Link href="/api/auth/signout">خروج</Link>}
-				{status === 'unauthenticated' && <Link href="/api/auth/signin">ورود</Link>}
-			</Box>
+						<ul className="flex gap-6">
+							{links.map((link) => (
+								<li key={link.href}>
+									<Link
+										href={link.href}
+										className={classnames({
+											'text-zinc-900': link.href === currentPath,
+											'text-zinc-500': link.href !== currentPath,
+											'hover:text-zinc-800 transition-colors': true
+										})}
+									>
+										{link.label}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</Flex>
+					<Box>
+						{status === 'authenticated' && (
+							<Link href="/api/auth/signout">خروج</Link>
+						)}
+						{status === 'unauthenticated' && (
+							<Link href="/api/auth/signin">ثبت نام / ورود</Link>
+						)}
+					</Box>
+				</Flex>
+			</Container>
 		</nav>
 	);
 };
