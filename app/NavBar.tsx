803ed8bb0b589022b184a16dd7d4d7f0ed1914f0
Hossein <rezaeig22@gmail.com>
2024-02-06 10:28:17 +0330

    56. Adding a Loading Skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,6 +6,7 @@ import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
+import { Skeleton, Spinner } from '@/app/components';
 
 const NavBar = () => {
 	return (
@@ -55,7 +56,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
 	const { status, data: session } = useSession();
 
-	if (status === 'loading') return null;
+	if (status === 'loading') return <Spinner />;
 
 	if (status === 'unauthenticated')
 		return (
